f06ed191301d6ff20f3aad571cadca092e72355d
SHARON ZHENG <sharzheng@gmail.com>
2019-02-16 21:43:52 -0500

    Updates

---
+++ precache-manifest.7a9229645124d1b7c93c17ca7d2ba1af.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b8162defe632c9fc448dcbb9aa64aab",
+    "url": "/ily/static/media/chocolate.2b8162de.gif"
+  },
+  {
+    "revision": "e98ff7eaf7bcdea7e86d",
+    "url": "/ily/static/css/main.a67f181d.chunk.css"
+  },
+  {
+    "revision": "26f42fd00cacae5d1070",
+    "url": "/ily/static/js/runtime~main.26f42fd0.js"
+  },
+  {
+    "revision": "1e3540e232687c2c56f6",
+    "url": "/ily/static/js/2.1e3540e2.chunk.js"
+  },
+  {
+    "revision": "9d8ce53a4153f81ec40ce51a3fd560ae",
+    "url": "/ily/static/media/stupid-cupid.9d8ce53a.gif"
+  },
+  {
+    "revision": "e98ff7eaf7bcdea7e86d",
+    "url": "/ily/static/js/main.e98ff7ea.chunk.js"
+  },
+  {
+    "revision": "c489f23ab6010bc7d78d6f5b8c9d3bf3",
+    "url": "/ily/static/media/flowers2.c489f23a.gif"
+  },
+  {
+    "revision": "db2251ae588dd27b450472b4c88bc4cd",
+    "url": "/ily/static/media/front.db2251ae.png"
+  },
+  {
+    "revision": "78eba2c7624ed228f8f2258dc76e8ab6",
+    "url": "/ily/static/media/flowers.78eba2c7.gif"
+  },
+  {
+    "revision": "19a8e4fe8653a16013b0e20b53813a7e",
+    "url": "/ily/static/media/Comic-Sans-MS.19a8e4fe.ttf"
+  },
+  {
+    "revision": "a2523643ee376cda50a4b59e4b504b0c",
+    "url": "/ily/index.html"
+  }
+];
