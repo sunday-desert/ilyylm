17cb0b614215558696ede83c5e0054b1eb38c6e2
sharon zheng <sharzheng@gmail.com>
2021-02-07 22:52:57 -0500

    use new domain

--- src/components/Title.js
+++ src/components/Title.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Title extends Component {
   render() {
     return (
-      <h1 style={this.props.style}><a href="https://sunday-desert.github.io/">ILYYL.M</a></h1>
+      <h1 style={this.props.style}><a href="https://sunday-desert.github.io/ilyylm">ILYYL.M</a></h1>
     );
   }
 }
