3818d384cd46e1f44fe19afc4da62131a3b50b7c
sharon zheng <sharzheng@gmail.com>
2021-02-07 22:30:26 -0500

    Updates

---
+++ precache-manifest.8855916753f6f2c1e1676d62009f2114.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "9a32dc3cc87153b2d5a2",
+    "url": "/ilyylm/static/css/main.4cd8740e.chunk.css"
+  },
+  {
+    "revision": "9a32dc3cc87153b2d5a2",
+    "url": "/ilyylm/static/js/main.9a32dc3c.chunk.js"
+  },
+  {
+    "revision": "3a44155c7332b706de66",
+    "url": "/ilyylm/static/js/runtime~main.3a44155c.js"
+  },
+  {
+    "revision": "45d50f831d4bedabca9f",
+    "url": "/ilyylm/static/js/2.45d50f83.chunk.js"
+  },
+  {
+    "revision": "9d8ce53a4153f81ec40ce51a3fd560ae",
+    "url": "/ilyylm/static/media/stupid-cupid.9d8ce53a.gif"
+  },
+  {
+    "revision": "2b8162defe632c9fc448dcbb9aa64aab",
+    "url": "/ilyylm/static/media/chocolate.2b8162de.gif"
+  },
+  {
+    "revision": "c489f23ab6010bc7d78d6f5b8c9d3bf3",
+    "url": "/ilyylm/static/media/flowers2.c489f23a.gif"
+  },
+  {
+    "revision": "db2251ae588dd27b450472b4c88bc4cd",
+    "url": "/ilyylm/static/media/front.db2251ae.png"
+  },
+  {
+    "revision": "78eba2c7624ed228f8f2258dc76e8ab6",
+    "url": "/ilyylm/static/media/flowers.78eba2c7.gif"
+  },
+  {
+    "revision": "19a8e4fe8653a16013b0e20b53813a7e",
+    "url": "/ilyylm/static/media/Comic-Sans-MS.19a8e4fe.ttf"
+  },
+  {
+    "revision": "6b6cbfc795c6f6208baf97c8d3e37d46",
+    "url": "/ilyylm/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c7dc67359350ad08f79ad6949dd7fc5f.js"
+  "/ilyylm/precache-manifest.8855916753f6f2c1e1676d62009f2114.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/ilyylm/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
